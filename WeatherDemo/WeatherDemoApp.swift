db54140ab616f4d33c3e431bbc438bfc4febf20b
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-12-19 18:36:44 +0500

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Arkasha Zuev on 19.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
